4883f2f83d00d74b76b97175bf4d83f72e39457d
ZanykStepan <stepan.zanyk.21@pnu.edu.ua>
2024-12-09 22:27:06 +0200

    Delete internetprogramming/lab5 directory

--- internetprogramming/lab5/script.js
+++
@@ -1,7 +0,0 @@
-function tabSwitch(new_tab, new_content) {
-    document.querySelectorAll('.tabs a').forEach(tab => tab.classList.remove('active'));
-    document.querySelectorAll('.content').forEach(content => content.classList.remove('active'));
-
-    document.getElementById(new_tab).classList.add('active');
-    document.getElementById(new_content).classList.add('active');
-}
